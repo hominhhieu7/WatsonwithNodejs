693e22add073b7afea654b56eade143ed1e5807d
Hieu <hominhhieu2502@gmail.com>
2019-05-13 01:17:13 +0700

    add PFD

--- service.js
+++ service.js
@@ -1,17 +1,18 @@
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
+
 const assistant = new AssistantV1({
     username: process.env.WATSON_USERNAME ,
     password: process.env.WATSON_PASSWORD,
     url: "https://gateway-tok.watsonplatform.net/assistant/api",
-    version: "2018-02-16"
+    version: "2019-02-01"
 })
 
 exports.getMessage = body => {
     return new Promise((resolve, reject) => {
         assistant.message({
             workspace_id: process.env.WATSON_WORKSPACE_ID,
-            input: { text: body.name }
+            input:{text: body.name}
         },
             function (err, response) {
                 if(err){
@@ -19,6 +20,8 @@ exports.getMessage = body => {
                     reject(err);
                 }
                 else {
+                    console.log(response.input);
+                    
                     resolve(response);
                 }
             }
